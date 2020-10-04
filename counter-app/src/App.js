b70a5c8458156edffeaafb21fcd8310c163f7c91
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-04 22:02:00 +0300

    init useEffect hook commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,43 +1,12 @@
-import React, { useState } from 'react';
+import React, {useState,useEffect} from 'react';
 
-function computeInitialCounter() {
-  console.log('Some calculations');
-  return Math.trunc(Math.random() * 20);
-}
 
 function App() {
-  // hook: useState()
-  const [counter, setCounter] = useState( () => computeInitialCounter() );
-
-  // object
-  const [state, setState] = useState( {
-    title: 'Counter',
-    date: Date.now()
-  });
-
-  function increment() {
-    setCounter( (prevState) => prevState+1);
-    // setCounter( (prevState) => prevState+1);
-  };
-
-  function decrement() {
-    setCounter( prevState => prevState-1);
-  };
-  function updateTitle() {
-    setState({
-      title: 'New title'
-    });
-  };
+  const [type, setType] = useState('users');
 
   return (
     <div>
-      <h1>Counter : {counter}</h1>
-      <button className="btn btn-success" onClick={increment}>Add</button>
-      <button className="btn btn-danger" onClick={decrement}>Delete</button> 
-
-      <pre>{JSON.stringify(state, null, 2)}</pre>
-      <button className="btn btn-default" onClick={updateTitle}>Rename title</button>
-
+      <h1>Resource: {type}</h1>
     </div>
   );
 }
---
+++ counter-app/src/examples/state.js
@@ -0,0 +1,52 @@
+// copy from App.js
+import React, { useState } from 'react';
+
+function computeInitialCounter() {
+  console.log('Some calculations');
+  return Math.trunc(Math.random() * 20);
+}
+
+function App() {
+  // hook: useState()
+  const [counter, setCounter] = useState( () => computeInitialCounter() );
+
+  // object
+  const [state, setState] = useState( {
+    title: 'Counter',
+    date: Date.now()
+  });
+
+  function increment() {
+    setCounter( (prevState) => prevState+1);
+    // setCounter( (prevState) => prevState+1);
+  };
+
+  function decrement() {
+    setCounter( prevState => prevState-1);
+  };
+  function updateTitle() {
+    // setState({
+    //   title: 'New title'
+    // });
+    setState( prev => {
+      return {
+        ...prev,
+        title: 'New title'
+      };
+    });
+  };
+
+  return (
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button className="btn btn-success" onClick={increment}>Add</button>
+      <button className="btn btn-danger" onClick={decrement}>Delete</button> 
+
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <button className="btn btn-default" onClick={updateTitle}>Rename title</button>
+
+    </div>
+  );
+}
+
+export default App;
