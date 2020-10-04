239a0635224828529bdda47fa1745990777bad06
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-04 22:54:00 +0300

    useEffect resource commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -4,13 +4,22 @@ import React, {useState,useEffect} from 'react';
 function App() {
   const [type, setType] = useState('users');
 
+  const [data, setData] = useState( [] );
+
+  useEffect( ()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${type}/`)
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, [type])
+
+
   return (
     <div>
       <h1>Resource: {type}</h1>
       <button onClick={() => setType('users')}>Users</button>
-      <button onClick={() => setType('todo')}>Todo</button>
+      <button onClick={() => setType('todos')}>Todos</button>
       <button onClick={() => setType('posts')}>Posts</button>
-
+  <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
