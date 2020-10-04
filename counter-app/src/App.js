6dc0c6cb3fa331938fbcaf3cc7f3707a2dd88bda
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-04 21:10:04 +0300

    add starter elements commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  // hook: useState()
+  const [counter, setCounter] = useState( 0 )
+  
   return (
     <div>
-      
+      <h1>Counter : {counter}</h1>
+      <button className="btn btn-success">Add</button>
+      <button className="btn btn-danger">Delete</button> 
     </div>
   );
 }
