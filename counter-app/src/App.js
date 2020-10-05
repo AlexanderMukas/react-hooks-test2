aeee84c4f1db999cb5dac31770348b3a7febc214
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-06 00:03:14 +0300

    initial for useMemo commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,44 +1,17 @@
-import React, {useState,useEffect, useRef} from 'react';
+import React, {useState} from 'react';
 
-// let renderCount = 1;
 function App() {
   
-  // Задача - посчитать сколько раз мы рендерили компонент
-  // const [renderCount, setRenderCount] = useState(1);
-  // ref часто используют для фокуса элемента
-
-  const [value, setValue] = useState('initial');
-
-  const renderCount = useRef(1);
-
-  const inputRef = useRef(null);
-
-  const focus = () => inputRef.current.focus();
-
-  const prevValue = useRef('');
-
-  useEffect( () => {
-    // setRenderCount( prev => prev +1);
-    renderCount.current++;
-    console.log(inputRef.current.value)
-
-  });
-
-  useEffect(  () => {
-    prevValue.current = value;
-  }, [value])
-
-
-  
+  // Задача - 
+  const [number, setNumber] = useState(42);
   
   return (
     <div>
-      <h1>Count of Render: {renderCount.current} </h1>
-      <h2>Prev value: {prevValue.current}</h2>
-      <input ref={inputRef} onChange={e => setValue(e.target.value)} value={value} />
+      <h1>Calculated property: {number}</h1>
+      <button className="btn btn-success" onClick={() => setNumber(prev => prev+1)}>Add</button>
+      <button className="btn btn-danger" onClick={() => setNumber(prev => prev-1)}>Delete</button>
 
-      <button className="btn btn-success" onClick={focus} >Focus</button>
-
+      
     </div>
   );
 }
