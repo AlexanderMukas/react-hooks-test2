d77719153d9e42259ab2eb196b567cd94e5b7ee1
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-05 23:57:42 +0300

    end of ref commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,25 +1,44 @@
 import React, {useState,useEffect, useRef} from 'react';
 
-let renderCount = 1;
+// let renderCount = 1;
 function App() {
   
   // Задача - посчитать сколько раз мы рендерили компонент
   // const [renderCount, setRenderCount] = useState(1);
-
+  // ref часто используют для фокуса элемента
 
   const [value, setValue] = useState('initial');
 
+  const renderCount = useRef(1);
+
+  const inputRef = useRef(null);
+
+  const focus = () => inputRef.current.focus();
+
+  const prevValue = useRef('');
+
   useEffect( () => {
     // setRenderCount( prev => prev +1);
-    renderCount++;
+    renderCount.current++;
+    console.log(inputRef.current.value)
 
   });
+
+  useEffect(  () => {
+    prevValue.current = value;
+  }, [value])
+
+
   
   
   return (
     <div>
-      <h1>Count of Render: {renderCount} </h1>
-      <input onChange={e => setValue(e.target.value)} value={value} />
+      <h1>Count of Render: {renderCount.current} </h1>
+      <h2>Prev value: {prevValue.current}</h2>
+      <input ref={inputRef} onChange={e => setValue(e.target.value)} value={value} />
+
+      <button className="btn btn-success" onClick={focus} >Focus</button>
+
     </div>
   );
 }
