7009355e7df8600cda991805c2ccb3610bfe4ba8
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-05 23:30:33 +0300

    infinite loop problem commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,10 +1,20 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState,useEffect, useRef} from 'react';
 
 
 function App() {
+  
+  // Задача - посчитать сколько раз мы рендерили компонент
+  const [renderCount, setRenderCount] = useState(1);
+
+  useEffect( () => {
+    setRenderCount( prev => prev +1);
+
+  });
+  
+  
   return (
     <div>
-      
+      <h1>Count of Render: {renderCount} </h1>
     </div>
   );
 }
