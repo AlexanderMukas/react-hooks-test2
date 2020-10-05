9847522273b3f01fa5028d4593bbf49c2b2982ad
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-05 23:36:56 +0300

    not good variant

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,13 +1,17 @@
 import React, {useState,useEffect, useRef} from 'react';
 
-
+let renderCount = 1;
 function App() {
   
   // Задача - посчитать сколько раз мы рендерили компонент
-  const [renderCount, setRenderCount] = useState(1);
+  // const [renderCount, setRenderCount] = useState(1);
+
+
+  const [value, setValue] = useState('initial');
 
   useEffect( () => {
-    setRenderCount( prev => prev +1);
+    // setRenderCount( prev => prev +1);
+    renderCount++;
 
   });
   
@@ -15,6 +19,7 @@ function App() {
   return (
     <div>
       <h1>Count of Render: {renderCount} </h1>
+      <input onChange={e => setValue(e.target.value)} value={value} />
     </div>
   );
 }
