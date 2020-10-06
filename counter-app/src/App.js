f866125643f0393bfa5063f5c1ec84f1d52274be
AlexanderMukas <alexandermukas@yahoo.com>
2020-10-06 23:48:55 +0300

    initial hook useCallback commit

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -4,10 +4,20 @@ function App() {
   
   // Задача useCallback
   
+  const [colored, setColored] = useState(false);
+  const [count, setCount] = useState(1);
+
+  const styles = {
+    color: colored ? 'blue' : 'violet'
+  }
+
   return (
-    <div>
+    <>
+      <h1 style={styles}>Count of properties: {count}</h1>
+      <button className="btn btn-success" onClick={() => setCount(prev => prev+1)}>Add</button>
+      <button className="btn btn-warning" onClick={() => setColored(prev => !prev)}>Change</button>
       
-    </div>
+    </>
   );
 }
 
